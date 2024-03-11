eb75e193c946e2b6d7afaf9b0ef50c8794a1d06e
Zaidan <148860414+Fachryzaidan-Akmal@users.noreply.github.com>
2024-03-11 18:03:57 +0700

    Create Poligon.java

---
+++ Praktikum 4/Poligon.java
@@ -0,0 +1,18 @@
+/*
+ Nama 		: Fachryzaidan Akmal
+ NIM		: 24060122120001
+ File		: Poligon.java
+ Deskripsi 	: Representasi dasar dari objek poligon(segi banyak)
+ Tanggal 	: 11 Maret 2024
+*/
+
+package org.poligon;
+
+public class Poligon{
+	protected int jumlahSisi;
+	
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+
+}
