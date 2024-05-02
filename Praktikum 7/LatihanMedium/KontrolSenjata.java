bc46bd2222fcba13c6ae3be1e826ad49923cc8b1
Zaidan <148860414+Fachryzaidan-Akmal@users.noreply.github.com>
2024-05-02 22:00:22 +0700

    Add files via upload

---
+++ Praktikum 7/LatihanMedium/KontrolSenjata.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LatihanMedium;
+
+/**
+ *
+ * @author Zaidan
+ */
+public class KontrolSenjata{
+    
+    Senjata senjata;
+    
+    public KontrolSenjata(Senjata senjata){
+       this.senjata = senjata;
+    }
+    
+    public boolean isAdaPeluru(){
+        return senjata.getPeluru() > 0;
+    }
+    
+    public void isiPeluru(int jumPeluru){
+        senjata.setPeluru(jumPeluru);
+        System.out.println(">> Peluru berhasil ditambah: "+jumPeluru);
+    }
+    
+    public void menembak(int jumlah) {
+        int jumlahPeluru = senjata.getPeluru();
+        System.out.println(">> Siap menembak "+jumlah+" kali");
+        if (senjata.getPeluru() == 0){
+                System.out.println("Peluru Habis");
+        } else {
+            for (int i = 0; i < jumlah; i++){
+                if (jumlahPeluru > 0){
+                    System.out.print(senjata.getBunyi()+" ");
+                    System.out.println("");
+                    jumlahPeluru--;
+                } else {
+                    System.out.println("Gagal tembak, Peluru habis");
+                }
+            }
+            senjata.setPeluru(jumlahPeluru); 
+            System.out.println(">> Sisa peluru: "+senjata.getPeluru());
+        }
+    }
+    
+    public String menusuk(){
+        if(senjata.isMenusuk()){
+            return "Jleb! ";
+        }
+        else {
+            return "Gagal, belum pasang bayonet";
+        }
+    }
+    
+    public void pasangBayonet(){
+        senjata.setMenusuk(true);
+        System.out.println("Bayonet Terpasang");
+    }
+}
+
